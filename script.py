f65ec74700cb8334b75d394105730a6e0726a3b7
Jeof Petty <jeofpetty@icloud.com>
2022-02-13 11:08:30 -0500

    main game works with bugs on input, inventory and being able to buy/sell more money/drugs than you have

--- script.py
+++ script.py
@@ -1,45 +1,44 @@
 #class Player: health, stash size, money,  # class Drugs: drugs, prices #title screen #instructions #class City #main screen with menu showing inv.. stash and current inv. #class LoanShark #class Police #random occurances #how long to win #
-import random
+from random import randrange, randint
 
 cities = ["West Palm Beach", "Boynton Beach",
           "Boca Raton", "Lake Worth", "Pahokee", "Belle Glade"]
-drugs_dict = {"cocaine": 0, "herion": 0,
-              "acid": 0, "weed": 0, "speed": 0, "ludes": 0}
 
 
 class Player:
-    day = 1
-    date = "Jan " + str(day) + ", 1984"
 
     def __init__(self):
         self.money = 2000
         self.trench_coat = 100
         self.health = 100
-        self.drugs = drugs_dict
-        self.stash = drugs_dict
+        self.day = 1
+        self.max_day = 31
+        self.drugs = {"cocaine": 0, "herion": 0,
+                      "acid": 0, "weed": 0, "speed": 0, "ludes": 0}
+        self.stash = {"cocaine": 0, "herion": 0,
+                      "acid": 0, "weed": 0, "speed": 0, "ludes": 0}
         self.guns = 0
         self.bank = 0
         self.debt = 5500
-        self.location = 0
+        self.location = "West Palm Beach"
         self.game_over = False
 
-    def switch_location(self, new_location):
+# switch_location takes in new_location, drugs object anc cartel object
+    def switch_location(self, new_location, drugs, cartel):
         if new_location != self.location:
             self.location = new_location
-            self.day += 1
-            if self.debt > 0:
-                self.debt = Cartel.interest_accumulation()
-                Cartel.loan_days += 1
+            drugs.cities_drug_prices()
+            self.advance_one_day(cartel)
 
     def add_drugs_to_trench_coat(self, drug, quantity):
         if self.trench_coat > 0 + quantity:
             self.trench_coat -= quantity
-            self.drugs[drug] += quantity
+            #self.drugs[drug] += quantity
 
     def take_drugs_from_trench_coat(self, drug, quantity):
         if self.trench_coat <= 100 - quantity:
             self.trench_coat += quantity
-            self.drugs[drug] -= quantity
+            #self.drugs[drug] -= quantity
 
     def buy_drugs(self, drug, quantity, cost_per_quantity):
         if drug not in self.drugs.keys():
@@ -88,26 +87,50 @@ class Player:
                 self.add_drugs_to_trench_coat(amount)
                 self.stash[drug] -= amount
 
+    def advance_one_day(self, cartel):
+        if self.day <= self.max_day:
+            self.day += 1
+            if self.debt > 0:
+                self.debt = cartel.interest_accumulation()
+                cartel.loan_days += 1
+        else:
+            self.game_over()
+
+    def game_over(self):
+        score = self.money * 2
+        self.game_over = True
+        return int(score / 1000000)
+
 
 # drugs is a random price between min and max, prices change in different cities and on different days.
 class Drugs:
-    cocaine = random.randint(15000, 30000)
-    herion = random.randint(5000, 14000)
-    acid = random.randint(1000, 4500)
-    weed = random.randint(300, 900)
-    speed = random.randint(70, 250)
-    ludes = random.randint(10, 60)
+    # drug_price_ranges from index 0: cocaine, herion, acid, weed, speed, ludes
+    drug_price_ranges = [[15000, 30000], [5000, 14000],
+                         [1000, 4500], [300, 900], [70, 250], [10, 60]]
 
-    def __init__(self, city):
-        self.city = ""
+    def __init__(self, current_city):
+        self.cities = cities
+        self.current_city = current_city
+        self.prices_in_city = {"cocaine": 0, "herion": 0,
+                               "acid": 0, "weed": 0, "speed": 0, "ludes": 0}
 
-# 6 cities to travel between, as you move to each city 1 game day will pass
+    def random_drug_price(self, drug, min, max):
+        if len(str(min)) >= 4:
+            price = randrange(min, max, 100)
+            self.prices_in_city[drug] = price
+        else:
+            price = randrange(min, max, 2)
+            self.prices_in_city[drug] = price
+        return price
 
+    def cities_drug_prices(self):
+        index = 0
+        for drug in self.prices_in_city.keys():
+            price = self.random_drug_price(
+                drug, self.drug_price_ranges[index][0], self.drug_price_ranges[index][1])
+            self.prices_in_city[drug] = price
+            index += 1
 
-class City:
-
-    def __init__(self):
-        pass
 
 # The Loan Sharks - will try to kill you after so many days of non payment, interest builds eavh day
 
@@ -141,13 +164,14 @@ class Cartel:
     def interest_accumulation(self):
         if self.loan_amount > 0:
             self.loan_amount += self.loan_amount * self.interest
+        return int(self.loan_amount)
 
 
 class Police:
 
     def __init__(self, name):
         self.name = name
-        self.deputies = random.randint(1, 9)
+        self.deputies = randint(1, 9)
         self.health = 100
 
 
@@ -191,10 +215,10 @@ def instructions(instructions_choice):
 def game_menu(user):
 
     print(
-        "\n     DATE: " + user.date + "                         TRENCH COAT POCKETS     " + str(user.trench_coat))
+        "\n     DATE: Jan {date}, 1984                         TRENCH COAT POCKETS     {pockets}".format(date=user.day, pockets=user.trench_coat))
     print("""
  ==============================================================================
-||       STASH       ||      WEST PALM BEACH     ||      TRENCH COAT         ||
+||       STASH       ||      """+user.location+"""     ||      TRENCH COAT         ||
 |=============================================================================|
 |       COCAINE                  """+str(user.stash["cocaine"])+""" ||      COCAINE                       """+str(user.drugs["cocaine"])+"""    |
 |       HERION                   """+str(user.stash["herion"])+""" ||      HERION                        """+str(user.drugs["herion"])+"""    |
@@ -206,7 +230,7 @@ def game_menu(user):
 |       BANK                     """+str(user.bank) + """ ||      GUNS                          """+str(user.guns)+"""    |
 |       DEBT                  """+str(user.debt)+""" ||      CASH                       """+str(user.money)+"""    |
  =============================================================================
-            """)
+            """.format(current_city=user.location))
 
 
 def cartel(user, cartel):
@@ -266,18 +290,95 @@ def bank(user):
             except:
                 print("\nNo withdrawl made.")
 
-
-def drug_menu(user):
-    pass
+# inputs player class as user and drug class as drugs
 
 
-#######
+def drug_menu(user, drugs, cartel):
+    cocaine = drugs.prices_in_city["cocaine"]
+    herion = drugs.prices_in_city["herion"]
+    acid = drugs.prices_in_city["acid"]
+    weed = drugs.prices_in_city["weed"]
+    speed = drugs.prices_in_city["speed"]
+    ludes = drugs.prices_in_city["ludes"]
+    print("\nHey dude, the prices of drugs here are:\n")
+    print("""
+                    COCAINE     {cocaine}             WEED    {weed}
+                    HERION      {herion}              SPEED   {speed}
+                    ACID        {acid}              LUDES   {ludes}
+    """.format(cocaine=cocaine, weed=weed, herion=herion, speed=speed, acid=acid, ludes=ludes))
+
+    choice = input("\nWill you buy, sell or jet? ")
+    while choice[0].lower() not in ['b', 's', 'j']:
+        choice = input("\nWill you buy, sell, or jet?(pick 'b', 's' or 'j') ")
+    if choice[0].lower() == 'b':
+        drug = input("\nWhat will you buy? ")
+
+        amount = input("\n How much {drug} will you buy?(Can afford {afford}) ".format(
+            drug=drug, afford=int(user.money / drugs.prices_in_city[drug])))
+        try:
+            amount = int(amount)
+            user.buy_drugs(drug, amount, drugs.prices_in_city[drug])
+        except:
+            print("\nNo transaction made")
+
+    if choice[0].lower() == 's':
+        drug = input("\nWhat will you sell? ")
+
+        amount = int(input("\nHow much {drug} you sell? ".format(drug=drug)))
+        try:
+            user.sell_drugs(drug, amount, drugs.prices_in_city[drug])
+        except:
+            print("\nNo sale was made")
+    if choice[0].lower() == 'j':
+        print("""\n
+Where to dude:      1) West Palm Beach      2) Boynton Beach        3) Boca Raton
+(Select 1 - 6)      4) Lake Worth           5) Pahokee              6) Belle Glade       
+        """)
+        travel = int(input("\nWhere do you want to go? "))
+        new_city = cities[travel - 1]
+        user.switch_location(new_city, drugs, cartel)
+        print(user.day)
+        print(user.trench_coat)
+    else:
+        pass
+
+
+def game(user, drugs, loan_shark):
+    already_visited = False
+    choice = title_screen()
+    instructions(choice)
+    while user.game_over == False:
+        if user.location == "West Palm Beach" and already_visited == False:
+            game_menu(user)
+            cartel(user, loan_shark)
+            game_menu(user)
+            stash(user)
+            game_menu(user)
+            bank(user)
+            already_visited = True
+        game_menu(user)
+        drug_menu(user, drugs, loan_shark)
+        if user.location != "West Palm Beach":
+            already_visited = False
+
+
+# Instantiate player, cartel and drugs
 user = Player()
 wendy = Cartel()
+drugs = Drugs("West Palm Beach")
+drugs.cities_drug_prices()
+#####################################
+
+game(user, drugs, wendy)
+
 #choice = title_screen()
 # instructions(choice)
-game_menu(user)
-cartel(user, wendy)
-bank(user)
+# game_menu(user)
+# stash(user)
+# bank(user)
 #user.game_over = False
-game_menu(user)
+# game_menu(user)
+#drug_menu(user, drugs)
+# game_menu(user)
+#drug_menu(user, drugs)
+# print(drugs.prices_in_city)
